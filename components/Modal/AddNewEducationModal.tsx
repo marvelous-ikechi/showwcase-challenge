35568895bb9894d04a8fcf1706136ff253585e94
marvelous-ikechi <mikechi@bscglobal.com>
2023-08-11 23:52:45 +0100

    setAppElement to new id
    Add handleRemarkField, handleRemarkFieldChang, and handleRemoveRemarkField functions.

--- components/Modal/AddNewEducationModal.tsx
+++ components/Modal/AddNewEducationModal.tsx
@@ -22,12 +22,57 @@ const customStyles = {
     transform: "translate(-50%, -50%)",
   },
 };
-Modal.setAppElement("#modal");
+
+Modal.setAppElement("#__next");
+
 function AddNewEducationModal({
   modalIsOpen,
   afterOpenModal,
   closeModal,
 }: Props) {
+  const [remarkFieldCount, setRemarkFieldCount] = React.useState(1);
+  const [remarkFields, setRemarkFields] = React.useState([
+    {
+      id: 1,
+      value: "",
+    },
+  ]);
+
+  const handleAddRemarkField = () => {
+    const newCount = remarkFieldCount + 1;
+
+    setRemarkFieldCount(newCount);
+    setRemarkFields([
+      ...remarkFields,
+      {
+        id: newCount,
+        value: "",
+      },
+    ]);
+  };
+
+  const handleRemarkFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const newRemarkFields = remarkFields.map((field) => {
+      if (field.id === Number(name)) {
+        return {
+          ...field,
+          value,
+        };
+      }
+      return field;
+    });
+    setRemarkFields(newRemarkFields);
+  };
+
+  const handleRemoveRemarkField = (id: number) => {
+    const newCount = remarkFieldCount - 1;
+
+    setRemarkFieldCount(newCount);
+    const newRemarkFields = remarkFields.filter((field) => field.id !== id);
+    setRemarkFields(newRemarkFields);
+  };
+
   return (
     <div id="modal">
       <Modal
@@ -40,16 +85,37 @@ function AddNewEducationModal({
         <div className="flex w-full items-end justify-end" onClick={closeModal}>
           <IoMdClose size={22} />
         </div>
-        <form className="pr-8 mt-4">
+        <div className="pr-8 mt-4">
           <ModalTextInput placeholder="Course studied" />
-          <ModalTextInput marginTop="1rem" placeholder="School attended" />
-
-          <ModalTextInput marginTop="1rem" placeholder="Remark" />
-          <RowWrapper marginTop="14px">
-            <FormButton>Add Remark</FormButton>
-            <FormButton margin="0 0 0 10px">Save</FormButton>
+          <ModalTextInput $marginTop="1rem" placeholder="School attended" />
+          {remarkFieldCount > 0 &&
+            remarkFields.map((field) => (
+              <RowWrapper $marginTop="1rem" key={field.id}>
+                <ModalTextInput
+                  placeholder="Remark"
+                  name={field.id.toString()}
+                  value={field.value}
+                  onChange={handleRemarkFieldChange}
+                />
+                {field.id !== 1 && (
+                  <IoMdClose
+                    size={22}
+                    onClick={() => handleRemoveRemarkField(field.id)}
+                  />
+                )}
+              </RowWrapper>
+            ))}
+          <RowWrapper $marginTop="14px">
+            {/* disable if we have up to 6 remarks */}
+            <FormButton
+              disabled={remarkFieldCount >= 6}
+              onClick={handleAddRemarkField}
+            >
+              Add Remark
+            </FormButton>
+            <FormButton $margin="0 0 0 10px">Save</FormButton>
           </RowWrapper>
-        </form>
+        </div>
       </Modal>
     </div>
   );
