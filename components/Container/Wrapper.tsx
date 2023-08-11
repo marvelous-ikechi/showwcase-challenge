5b6bfca12d81644e6e1b713a9dd26dda631f702f
marvelous-ikechi <mikechi@bscglobal.com>
2023-08-11 21:58:53 +0100

    Rowwrapper added

--- components/Container/Wrapper.tsx
+++ components/Container/Wrapper.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface RowWrapperProps {
+  marginTop?: string;
+}
+
 export const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -14,4 +18,12 @@ export const SectionWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+export const RowWrapper = styled.div<RowWrapperProps>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: ${(props) => props.marginTop || "0px"};
 `;
