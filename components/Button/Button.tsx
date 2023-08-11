22bc20a538b1741087033477ee87b113b04bdcef
marvelous-ikechi <mikechi@bscglobal.com>
2023-08-11 15:49:27 +0100

    GeneralButton component added.

---
+++ components/Button/Button.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const SubmitButton = styled.button`
+  background-color: #f5f5f5;
+  border: 1px solid #e6e6e6;
+  border-radius: 3px;
+  color: #262626;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 600;
+  padding: 8px 9px;
+  text-align: center;
+  width: 100%;
+  &:disabled {
+    opacity: 0.3;
+  }
+`;
+
+export const GeneralButton = styled.button`
+  background-color: #3897f0;
+  border: 1px solid #3897f0;
+  border-radius: 3px;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 600;
+  padding: 8px 9px;
+  text-align: center;
+  width: 50%;
+  &:disabled {
+    opacity: 0.3;
+  }
+`;
