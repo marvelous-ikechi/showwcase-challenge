9f722242970f571c6dea28206a05ea89e8d9b17c
marvelous-ikechi <mikechi@bscglobal.com>
2023-08-11 22:12:01 +0100

    Form styled component button added

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+interface FormButtonProps {
+  width?: string;
+  margin?: string;
+}
+
 export const SubmitButton = styled.button`
   background-color: #f5f5f5;
   border: 1px solid #e6e6e6;
@@ -27,6 +32,23 @@ export const GeneralButton = styled.button`
   padding: 8px 9px;
   text-align: center;
   width: 50%;
+  &:disabled {
+    opacity: 0.3;
+  }
+`;
+
+export const FormButton = styled.button<FormButtonProps>`
+  background-color: #3897f0;
+  border: 1px solid #3897f0;
+  border-radius: 3px;
+  color: #ffffff;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 600;
+  padding: 8px 9px;
+  text-align: center;
+  margin: ${(props: FormButtonProps) => props.margin ?? "0"};
+  width: ${(props: FormButtonProps) => props.width ?? "100%"};
   &:disabled {
     opacity: 0.3;
   }
