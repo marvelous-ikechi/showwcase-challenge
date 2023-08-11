2578a83962845baa43bdfe759f89cd510c3e7937
marvelous-ikechi <mikechi@bscglobal.com>
2023-08-11 15:45:29 +0100

    handle users-input; navigate to Main screen if users input is not empty.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,42 @@
 import type { NextPage } from "next";
-import { SubmitButton } from "../components/Button/SubmitBtn";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+import { SubmitButton } from "../components/Button/Button";
 import { TextInput } from "../components/TextInput/TextInput";
 import { Heading } from "../components/Typography/Heading";
 import { Text } from "../components/Typography/Text";
 import { PageWrapper, SectionWrapper } from "../components/Container/Wrapper";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const [name, setName] = useState<string>("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setName(value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    router.push({
+      pathname: "/main",
+      query: { name: name },
+    });
+  };
+
   return (
     <PageWrapper>
       <Heading>Hi there! Welcome to your education showcase</Heading>
       <SectionWrapper>
         <Text>Type your name and click &quot;Enter&quot; below to begin</Text>
-        <TextInput placeholder="Enter your name" />
-        <SubmitButton>Enter</SubmitButton>
+        <form onSubmit={handleSubmit}>
+          <TextInput onChange={handleChange} placeholder="Enter your name" />
+          {/* Disable button if name is empty */}
+          <SubmitButton type="submit" disabled={name.length < 1}>
+            Enter
+          </SubmitButton>
+        </form>
       </SectionWrapper>
     </PageWrapper>
   );
