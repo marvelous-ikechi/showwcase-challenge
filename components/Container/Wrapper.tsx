095d7d8fa4886c7a407dbcabeea6cc215da3cc68
marvelous-ikechi <mikechi@bscglobal.com>
2023-08-11 15:13:31 +0100

    Wrapper component created.

---
+++ components/Container/Wrapper.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const SectionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
