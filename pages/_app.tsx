7dcb8adfcb50b21a37d5820d55b1e251fd2a5d32
marvelous-ikechi <mikechi@bscglobal.com>
2023-08-12 18:01:48 +0100

    Student provider wrapped around entry component.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StudentProvider } from "./context/StudentContext";
 
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <StudentProvider>
+        <Component {...pageProps} />
+      </StudentProvider>
     </QueryClientProvider>
   );
 }
