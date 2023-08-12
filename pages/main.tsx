189c322fe5cab5a6ea51b547ae6b333dbb33183b
marvelous-ikechi <mikechi@bscglobal.com>
2023-08-12 18:04:50 +0100

    rought template for rendering students data.

--- pages/main.tsx
+++ pages/main.tsx
@@ -5,12 +5,14 @@ import { Text } from "../components/Typography/Text";
 import { useRouter } from "next/router";
 import { GeneralButton } from "../components/Button/Button";
 import AddNewEducationModal from "../components/Modal/AddNewEducationModal";
+import { useStudentsContext } from "./context/StudentContext";
 
 const Main: React.FC = () => {
   const router = useRouter();
   const { name } = router.query;
   const user = name ?? "";
 
+  const { students, addStudentData } = useStudentsContext();
   const [isModalOpen, setIsModalOpen] = React.useState(false);
   const afterOpenModal = () => {};
   const closeModal = () => {
@@ -18,7 +20,13 @@ const Main: React.FC = () => {
   };
 
   return (
-    <PageWrapper id="main">
+    <div
+      className={`bg-black pt-8 flex flex-1 flex-col h-screen  items-center
+        ${students?.length === 0 && "justify-center "}
+      
+      w-screen`}
+      id="main"
+    >
       <Text>
         Welcome to {user} {"'s"} education page
       </Text>
@@ -36,7 +44,34 @@ const Main: React.FC = () => {
           closeModal={closeModal}
         />
       </SectionWrapper>
-    </PageWrapper>
+      <SectionWrapper
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {students?.map((student, key) => (
+          <div
+            key={key}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              border: "1px solid black",
+              padding: "10px",
+              margin: "10px",
+            }}
+          >
+            <Text>{student.school}</Text>
+            <Text>{student.course}</Text>
+            {student.remarks.map((remark) => (
+              <Text key={remark}>{remark}</Text>
+            ))}
+          </div>
+        ))}
+      </SectionWrapper>
+    </div>
   );
 };
 
