053f0f0ae2e0440a7d6d66b79e73467d301acbe8
marvelous-ikechi <mikechi@bscglobal.com>
2023-08-12 17:10:32 +0100

    handle when dropdown should be open or closed.

--- components/Dropdown/SeachDropDown.tsx
+++ components/Dropdown/SeachDropDown.tsx
@@ -6,10 +6,13 @@ function SeachDropDown() {
   const [school, setSchool] = React.useState<string>("");
   const handleSchoolChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSchool(e.target.value);
+    setCloseDropDown(false);
   };
+  const [closeDropdown, setCloseDropDown] = React.useState<boolean>(false);
 
   const handleSelectChange: React.MouseEventHandler<HTMLDivElement> = (e) => {
     setSchool(e.currentTarget.textContent || "");
+    setCloseDropDown(true);
   };
 
   const { data: schools, isLoading, isError } = useFetchSchools(school);
@@ -22,17 +25,19 @@ function SeachDropDown() {
         $marginTop="1rem"
         placeholder="School attended"
       />
-      <div className="absolute bg-white w-full h-auto mt-1 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline overflow-y-scroll max-h-32">
-        {schools?.map((school: any, key: any) => (
-          <div
-            onClick={handleSelectChange}
-            className="mt-2 border-b p-2 cursor-pointer"
-            key={key}
-          >
-            {school.name}
-          </div>
-        ))}
-      </div>
+      {schools && school.length > 0 && !closeDropdown && (
+        <div className="absolute bg-white w-full h-auto mt-1 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline overflow-y-scroll max-h-32">
+          {schools?.map((school: any, key: any) => (
+            <div
+              onClick={handleSelectChange}
+              className="mt-2 border-b p-2 cursor-pointer"
+              key={key}
+            >
+              {school.name}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
